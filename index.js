f9227690c6b71fcea9060c69931467de35375a84
Rizal <rizalnug2@gmail.com>
2025-01-02 22:46:52 +0700

    restful update product

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const express = require("express");
+//Menggunakan method override agar bisa menggunakan method put dan catch
+const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const app = express();
 
@@ -20,6 +22,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 //Untuk save wajib urlencoded
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method")); //untuk method put
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -50,6 +53,14 @@ app.get("/products/:id/edit", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
   res.render("products/edit", { product });
+});
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+  });
+  res.redirect(`/products/${product._id}`);
 });
 
 app.listen(3000, () => {
