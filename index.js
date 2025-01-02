4bc7b1e67432a25a022721c75d978ef4787202d0
Rizal <rizalnug2@gmail.com>
2025-01-02 22:51:56 +0700

    restful delete

--- index.js
+++ index.js
@@ -63,6 +63,12 @@ app.put("/products/:id", async (req, res) => {
   res.redirect(`/products/${product._id}`);
 });
 
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndDelete(id);
+  res.redirect("/products");
+});
+
 app.listen(3000, () => {
   console.log("Server is running on http://127.0.0.1:3000");
 });
