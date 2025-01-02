906ebea3c1624b8094e0b569b94aacff2d71780c
Rizal <rizalnug2@gmail.com>
2025-01-02 14:32:32 +0700

    first setup

---
+++ index.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+// Connect to MongoDB
+mongoose
+  .connect("mongodb://127.0.0.1/shop_db")
+  .then((result) => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://127.0.0.1:3000");
+});
